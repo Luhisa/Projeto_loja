aaf2089d40770fd36506dd824ae2df1e2a35c412
Luhisa <luisavtrsantos@gmail.com>
2023-04-25 11:18:02 -0300

    Atualização 25/04

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+//lista dos modelos
+modeloJson.map((item, index) => {
+    let modeloItem = c('.models .modelo-item').cloneNode(true); //consulta
+
+    modeloItem.querySelector('.modelo-item-img img').src = item.img;
+    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
+    modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    
+
+    c('.modelo-area').append(modeloItem);
+});
+
+
+
