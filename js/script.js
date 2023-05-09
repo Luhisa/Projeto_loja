b9d057702f49f6553e03c692881fb503bdae3b46
Luhisa <luisavtrsantos@gmail.com>
2023-05-09 11:15:12 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
-//lista dos modelos
+// Listagem dos modelos
+
 modeloJson.map((item, index) => {
-    let modeloItem = c('.models .modelo-item').cloneNode(true); //consulta
+let modeloItem = c('.models .modelo-item').cloneNode(true);
 
-    modeloItem.querySelector('.modelo-item-img img').src = item.img;
-    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
-    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
-    modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+modeloItem.setAttribute('data-key', index);
 
-    modeloItem.querySelector('a').addEventListener('click', (e) => { e.preventDefault();
+modeloItem.querySelector('.modelo-item-img img').src = item.img;
+modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
+modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
 
-        c('.modeloWindowArea').style.display = 'flex';
+modeloItem.querySelector('a').addEventListener('click', (e) => {
+e.preventDefault();
 
-    })
+let key = e.target.closest('.modelo-item').getAttribute('data-key');
 
-    c('.modelo-area').append(modeloItem);
+c('.modeloBig img').src = modeloJson[key].img;
+c('.modeloInfo h1').innerHTML = modeloJson[key].name;
+c('.modeloInfo-desc').innerHTML = modeloJson[key].description;
+c('.modeloInfo-actualPrice').innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+
+cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeIndex];
+});
+
+c('.modeloWindowArea').style.opacity = 0;
+c('.modeloWindowArea').style.display = 'flex';
+
+setTimeout(() => {
+c('.modeloWindowArea').style.opacity = 1;
+}, 200);
+});
+
+c('.modelo-area').append(modeloItem);
 });
 
 
